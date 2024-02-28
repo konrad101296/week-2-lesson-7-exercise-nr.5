d3f2124aba82343f2e4289f3f087e62ff175cc41
konrad101296 <konradslimak@o2.pl>
2024-02-28 20:14:13 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.5/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("podaj swoj wiek liczbą: ");
+int Wiek = int.Parse(Console.ReadLine());
+
+if (Wiek > 20 && Wiek < 30)
+{
+    Console.WriteLine("Możesz zostac posłem");
+    Console.WriteLine("Mozesz zostac premierem");
+}
+else if (Wiek > 29 && Wiek < 35)
+{
+    Console.WriteLine("Możesz zostac senatorem");
+    Console.WriteLine("Możesz zostac posłem");
+    Console.WriteLine("Mozesz zostac premierem");
+}
+else if (Wiek > 34)
+{
+    Console.WriteLine("Możesz zostac senatorem");
+    Console.WriteLine("Możesz zostac posłem");
+    Console.WriteLine("Mozesz zostac premierem");
+    Console.WriteLine("Możesz zostac prezydentem");
+}
+else
+{
+    Console.WriteLine("Jestes za młody");
+}
